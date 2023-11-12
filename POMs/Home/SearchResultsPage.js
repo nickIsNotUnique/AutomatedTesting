c185fe16411480413baf7a7d72d6340a50caf9e4
Daria Domina <dariadomina@cloud.upwork.com>
2023-11-12 19:22:01 +0100

    smoke search test: first and second step

--- POMs/Home/SearchResultsPage.js
+++ POMs/Home/SearchResultsPage.js
@@ -4,6 +4,22 @@ export class SearchResultsPage {
     constructor(page) {
         this.page = page
 
-        // this.searchResults = this.page.locator('[data-testid="object-list-display-options-container"] ~ div > div > div').all()
+        this.searchResults = this.page.locator('[data-testid="object-list-display-options-container"] ~ div > div > div').all()
+        this.linkToLot = this.page.locator('div > a')
+    }
+
+    async selectSearchResult(position){
+        return this.searchResults[position-1]
+    }
+
+    async openLotDetails(searchResult){
+        await test.step(`Open the Details for the lot #${this.searchResults.indexOf(searchResult)+1}`, () => {})
+        let link = await searchResult.locator(this.linkToLot).getAttribute('href')
+
+        const detailsRequest = this.page.waitForResponse(
+            response => response.url() === link && response.status() === 200
+        )
+        await searchResult.click()
+        await detailsRequest
     }
 }
--- tests/catawiki.spec.js
+++ tests/catawiki.spec.js
@@ -2,22 +2,21 @@ const { test, expect } = require('./../fixtures.js');
 
 /** @type String */ let lot
 /** @type Number */ let position
+/** @type Array */ let details
 test('Smoke test: Search for a Lot', async ({ pagesManager }) => {
     lot = 'train'
     position = 2
+    details = ['lotName', 'favouriteCounter', 'currentBid']
 
     await test.step(`Search for the ${lot} lot from the Home Page`, async () => {
         await pagesManager.homePage.searchByKeyword(lot)
     })
 
-    await test.step(`Check that the Search Results Page had been opened`, async () => {
-        console.log('Test settings')
+    await test.step(`Open the Lot Details Page for ${position} lot from the Search Results Page`, async () => {
+        let result = await pagesManager.searchResults.selectSearchResult(position)
+        await pagesManager.searchResults.openLotDetails(result)
     })
 
-    // await test.step(`Check that the Search Results Page contains the corresponding results`, async () => {
-    // })
-
-    await test.step(`Open the Lot Details Page for ${position} lot from the Search Results Page`, async () => {
-        console.log('Test settings')
+    await test.step(`Verify the set of details of the lot: ${details}`, async () => {
     })
 })
